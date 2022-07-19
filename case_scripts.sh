b2dd909344a05e5d19d96fd609594824f116f775
franckfongang <franckfongang1980@gmail.com>
2022-07-19 02:59:05 -0400

    add files

---
+++ case_scripts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo
+echo Please chose one of the options below 
+echo
+echo 'a = Display Date and Time'
+echo 'b = List file and directories'
+echo 'c = List users logged in'
+echo 'd = Check System uptime'
+echo 
+
+       read choises 
+       
+       case $choices in
+
+a)date;;
+b)ls;;
+c)who;;
+d)uptime;;
+echo Invalid choice - Bye.
---
+++ comment.sh
@@ -0,0 +1,2 @@
+#Most of the installation programs are written in case statements where the scripts waits 
+#for user input to select from choices.
